638dcfe8a436b05cffe5ace2562aeabc0d56e641
mohammadgdz <mhd.gdz@gmail.com>
2023-08-19 18:57:34 +0330

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mohammad on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
